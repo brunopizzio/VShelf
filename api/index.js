3806ad9d75f0e834e12905cfe465fce7ec033585
brunopizzio <brunopizzio7@gmail.com>
2019-02-16 00:24:50 -0200

    API Update

--- api/index.js
+++ api/index.js
@@ -31,6 +31,8 @@ router.get('/' , function(req, res) {
 // Rotas que terminarem com '/books' (GET ALL & POST)
 
 router.route('/books')
+
+// Cadastras livro
 .post(function(req, res) {
     const book = new Book();
     book.titulo = req.body.titulo;
@@ -44,6 +46,60 @@ router.route('/books')
         res.json({message: 'Livro cadastrado com sucesso!'}); 
     });
 })
+
+// Selecionar todos os livros
+.get(function(req, res) {
+    Book.find(function(error, books){
+        if(error)
+            res.send('Erro ao tentar selecionas todos os livros'+error)
+        res.json(books);
+    });
+});
+
+// Rotas que terminarem com '/books/:book_id' (GET, PUT & DELETE)
+
+router.route('/books/:book_id')
+
+// Selecionar livro por ID
+.get(function (req, res) {
+    Book.findById(req.params.book_id, function(error, book){
+        if(error)
+            res.send('ID do livro não encontrado.'+error);
+        res.json(book);
+    });    
+})
+
+// Atualizar livro por ID
+.put(function (req, res) {
+    Book.findById(req.params.book_id, function(error, book){
+        if(error)
+            res.send('ID do livro não encontrado.'+error);
+        
+            book.titulo = req.body.titulo;
+            book.autor = req.body.autor;
+            book.paginas = req.body.paginas;
+            book.descricao = req.body.descricao;
+
+            book.save(function(error){
+                if(erro)
+                    res.send('Erro ao atualizar o livro.'+error);
+                res.json({ message: 'Produto atualizado com sucesso!'});
+            });
+        });
+    })
+
+    .delete(function(req, res){
+        Book.remove({
+            _id: req.params.book_id
+            }, function(error){
+                if(error)
+                    res.send('ID do produto não encontrado'+error);
+                res.json({ message: 'Livro deletado com sucesso!'});
+            });
+         });
+
+
+
 
 // Definindo um padrão das rotas prefixadas: '/api'
 app.use('/api', router);
